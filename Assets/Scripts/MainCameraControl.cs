3adc41a83be8adffdbf99864aad4917d7fd5dfbc
umetaman <info@umetaman.com>
2019-09-03 14:36:32 +0900

    実験用のVFXを作成

---
+++ Assets/Scripts/MainCameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MainCameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private GameObject targetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            Debug.LogError("MainCamera is Null");
+        }
+
+        if(targetObject == null)
+        {
+            Debug.LogError("Target is Null.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.LookAt(targetObject.transform);
+    }
+}
